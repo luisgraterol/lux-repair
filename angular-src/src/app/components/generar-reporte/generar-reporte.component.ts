9c945c5c1bf64e099c008165f07d64e6d2dcc9a9
slossada <santilossada@gmail.com>
2018-04-01 23:56:42 -0400

    Iconos del dash

--- angular-src/src/app/components/generar-reporte/generar-reporte.component.ts
+++ angular-src/src/app/components/generar-reporte/generar-reporte.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+
+// HTTP Requests
+import { Http, Headers } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-generar-reporte',
@@ -7,9 +13,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GenerarReporteComponent implements OnInit {
 
-  constructor() { }
+  rol: any;
+
+  constructor(
+    private http: Http,
+    private authService: AuthService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
-  }
 
+    let headers = new Headers();
+
+    // Fetches the token of the currently logged in user from localStorage
+    headers.append('Authorization', localStorage.getItem('id_token'));
+    headers.append('Content-Type', 'application/json');
+
+    this.http.get('http://localhost:3000/users/profile', { headers })
+      .map(res => res.json())
+      .subscribe(profile => {
+        this.rol = profile.user.rol;
+      }, err => {
+        console.log('Error while getting the profile: ', err);
+        return false;
+      });
+  }
 }
