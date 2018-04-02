234e202ec28e2f814d39ecc0b1f27b117b386280
slossada <santilossada@gmail.com>
2018-04-02 01:26:36 -0400

    Lista de vehiculos para generar reporte

--- angular-src/src/app/components/reporte-vehiculo/reporte-vehiculo.component.ts
+++ angular-src/src/app/components/reporte-vehiculo/reporte-vehiculo.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { Router } from '@angular/router';
+import { DatePipe } from '@angular/common';
+import { Http, Headers } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-reporte-vehiculo',
@@ -7,9 +12,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReporteVehiculoComponent implements OnInit {
 
-  constructor() { }
+  vehiculos: any[];
+  clientes: any[];
+
+  constructor(
+    private http: Http,
+    private flashMessage: FlashMessagesService,
+    private router: Router,
+    private datePipe: DatePipe
+  ) {}
 
   ngOnInit() {
+    let headers = new Headers();
+
+    // Busca el token del usuario que esta ingresado en el sistema actualmente
+    const token = localStorage.getItem('id_token');
+
+    // Settear los encabezados para la petición al API
+    headers.append('Authorization', token);
+    headers.append('Content-Type', 'application/json');
+
+    this.http.get('http://localhost:3000/users/vehiculos-gerente-cliente', { headers })
+      .map(res => res.json())
+      .subscribe(data => {
+        data.vehiculos.map(vehiculo => {});
+
+        this.vehiculos = data.vehiculos;
+        console.log(this.vehiculos);
+
+        localStorage.setItem('vehiculos', JSON.stringify(data.vehiculos));
+      }, err => {
+        console.log('Error al pedir los vehiculos desde ReporteVehiculoComponent: ', err);
+        return false;
+      });
+    
+  }
+
+  generar(indice) {
+    localStorage.setItem('vehiculo-reporte', indice);
+    localStorage.setItem('ultima-pagina', '/reporte-vehiculo');
+    this.router.navigate(['/detalle-vehiculo']);
   }
 
 }
--- controllers/gerente.js
+++ controllers/gerente.js
@@ -123,4 +123,52 @@ controller.getVehiculosConMecanico = async function (callback) {
     }
 };
 
+// Metodo que retorna un arreglo de vehiculos con todos los datos pertinentes
+controller.getVehiculosSinFiltro = async function (callback) {
+    try {
+        let response = await Vehiculo.findAll({ where: { Activo: true } });
+
+        // Construye un arreglo unicamente con los datos necesarios
+        let vehiculos = response.map(resultado => resultado.dataValues);
+
+        // Busca los nombres de las marcas de los vehiculos
+        for (let i = 0; i < vehiculos.length; i++) {
+            let response = await Marca.findById(vehiculos[i].Marca);
+            vehiculos[i].Marca = response.dataValues.Nombre;
+        }
+
+        // Busca los nombres de los modelos de los vehiculos
+        for (let i = 0; i < vehiculos.length; i++) {
+            let response = await Modelo.findById(vehiculos[i].Modelo);
+            vehiculos[i].Modelo = response.dataValues.Nombre;
+        }
+
+        // Agrega los datos de la orden de cada vehiculo, en el caso de tenerla
+        for (let i = 0; i < vehiculos.length; i++) {
+            let response = await Orden.findOne({
+                where: {
+                    Vehiculo: vehiculos[i].id,
+                    Activa: true
+                }
+            });
+            if (!!response) { // Solo agrega los datos si ese vehiculo tiene una orden activa
+                vehiculos[i].Estado = response.dataValues.Estado;
+                vehiculos[i].Evaluacion = response.dataValues.Evaluacion;
+                vehiculos[i].Servicio = response.dataValues.Servicio;
+                vehiculos[i].FechaSolicitud = response.dataValues.FechaSolicitud;
+                vehiculos[i].FechaAdmision = response.dataValues.FechaAdmision;
+                vehiculos[i].DetalleOrden = response.dataValues.Detalle;
+                vehiculos[i].codigoQR = response.dataValues.QR;
+                vehiculos[i].Imagen = response.dataValues.Imagen;
+            }
+        }
+
+        // Retorna el arreglo
+        callback(vehiculos, null);
+    } catch (err) {
+        callback(null, err);
+    }
+};
+
+
 module.exports = controller;
